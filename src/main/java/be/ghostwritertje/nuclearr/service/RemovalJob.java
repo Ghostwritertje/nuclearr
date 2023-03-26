b78a009668b15bc67d1f41f06b9138677f3e62b6
Ghostwritertje <ghostwritertje@joran.app>
2023-03-26 09:03:15 +0200

    re-enable automatic removal job

--- src/main/java/be/ghostwritertje/nuclearr/service/RemovalJob.java
+++ src/main/java/be/ghostwritertje/nuclearr/service/RemovalJob.java
@@ -1,5 +1,6 @@
 package be.ghostwritertje.nuclearr.service;
 
+import be.ghostwritertje.nuclearr.hardlinks.HardlinkService;
 import be.ghostwritertje.nuclearr.torrent.TorrentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,14 +16,16 @@ public class RemovalJob {
 
     private final TorrentRemovalService torrentRemovalService;
     private final TorrentImporterService torrentImporterService;
+    private final HardlinkService hardlinkService;
     private final TorrentService torrentService;
 
-//    @Scheduled(fixedDelay = 3600, initialDelay = 1, timeUnit = TimeUnit.SECONDS)
+    @Scheduled(fixedDelay = 3600, initialDelay = 60, timeUnit = TimeUnit.SECONDS)
     public void scheduled() {
         log.info("deleting torrents");
 
         torrentService.deleteAll()
                 .then(torrentImporterService.importTorrents())
+                .then(hardlinkService.updateAllHardlinks())
                 .then(torrentRemovalService.removeTorrents())
                 .subscribe(ignored -> log.debug("DELETED!"), ignored -> log.error("failed job", ignored),
                         () -> log.info("finished job"));
